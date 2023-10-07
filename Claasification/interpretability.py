64d24c4796373313299927039f92451aa2f6881b
Emmanuel AGOSSOU <agossouema2011@gmail.com>
2023-10-07 12:28:18 +0200

    update Lime images

--- Claasification/interpretability.py
+++ Claasification/interpretability.py
@@ -3,7 +3,8 @@ import torch
 import argparse
 import torch.nn as nn
 import torch.optim as optim
-
+from torchvision import models
+from numpy import asarray
 from model import build_model
 from datasets import get_datasets
 from utils import save_model, save_plots
@@ -24,50 +25,42 @@ from swin_transformer_pytorch.swin_transformer import SwinTransformer
 #explainer = lime_image.LimeImageExplainer()
 
 CLASS_NAMES = ['Bleeding', 'Non-Bleeding']    
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+DEVICE='cpu'
 
-# Load the model
 checkpoint = torch.load(os.path.join('outputs', 'model.pth'))
+    
 # Load the model.
 model = build_model(
                     num_classes=len(CLASS_NAMES)
                     ).to(DEVICE)
     
-model.load_state_dict(checkpoint['model_state_dict']) # loda the saved model
-    
-model.eval()
-    
-
-def plotshap(model,test_images,background):
-    """
-    explainer = shap.DeepExplainer(model, image)
-    shap_values = explainer.shap_values(image)
-    #explainer = shap.TreeExplainer(model)
-    #shap_values = explainer.shap_values(X_test)
-    print("Variable Importance Plot - Global Interpretation")
-    figure = plt.figure()
-    shap.summary_plot(shap_values, image)
-    shap.summary_plot(shap_values[0], image)
-    plt.savefig('outputs/shape.png')
-    
-    """
-    print("----------------------------------Welcome to Interpretability ----------------------------\n")
-    e = shap.DeepExplainer(model, background)
-    print("Shape:",background.shape)
-    print(test_images.shape)
-    shap_values = e.shap_values(test_images)
-    shap_numpy = [np.swapaxes(np.swapaxes(s, 1, -1), 1, 2) for s in shap_values]
-    test_numpy = np.swapaxes(np.swapaxes(test_images.cpu().numpy(), 1, -1), 1, 2)
-    shap.image_plot(shap_numpy, -test_numpy)
-
-
-def classifier_fn (numpy_image):
-    torch_image = torch.from_numpy(numpy_image)
+model.load_state_dict(checkpoint['model_state_dict']) # load the saved model
+ 
+def model_inference(numpy_array):
+    print("-------------------------------------------------------------") 
+    torch_image = torch.from_numpy(numpy_array)
+    torch_image = torch_image.permute(0,3,1,2)
+    model.to(torch.double)
     with torch.no_grad():
-        outputs = model (torch_image)
-        _ , predicted_class = torch.max(outputs, 1)
-        probs = F.softmax(predicted_class, dim=1)
-    return probs.detach().cpu().numpy()
+        output = model(torch_image.to(DEVICE))
+        
+        # Softmax probabilities.
+        predictions = torch.softmax(output, dim=1).cpu().numpy()
+        # Predicted class number.
+        output_class = np.argmax(predictions)
+                
+        print("predictions:",predictions)
+        #print("output_class:",output_class)
+        print("Classes:",CLASS_NAMES[output_class])
+        
+        #_, predicted_class = torch.max(output, 1)
+    #probs = F.softmax(predicted_class.float(), dim=0)
+    #print("predicted_class:",predicted_class)
+    #return probs.detach().cpu().numpy().tolist()
+    #return CLASS_NAMES[output_class] 
+    return predictions
+        
     
 
 if __name__ == '__main__':
@@ -78,17 +71,9 @@ if __name__ == '__main__':
                           dataset_valid,
                           batch_size=batch_size)
    
-    """
-    batch = next(iter(validloader))
-    images, _ = batch
     
-    #background = images[:1].to(DEVICE)
-    #test_images = images[1:10].to(DEVICE)
-    #plotshap(model,test_images,background) # Call to plot intepretability SHAP for the 10 first images
-    """  
-   
     #define the image and proceed with classification and interpretability
-    image_path = "input/Validation/Bleeding/img- (10).png"
+    image_path = "input/Test/TestDataset2/A0216.png"
     input_size = 224
     test_transform = transforms.Compose([transforms.CenterCrop(input_size),
     transforms.ToTensor(),
@@ -99,22 +84,28 @@ if __name__ == '__main__':
     
     print ("Image shape:",input_image.shape)
     input_image_numpy = input_image[0].permute(1,2,0).numpy()
+    print ("Image shape1:",input_image_numpy.shape)
     input_image_numpy= input_image_numpy.astype("double")
-
+    print ("Image shape2:",input_image_numpy.shape)
+    
     explainer = lime_image.LimeImageExplainer()  
-    explanation = explainer.explain_instance(input_image_numpy, classifier_fn, top_labels=5, num_samples=1000)
+    explanation = explainer.explain_instance(input_image_numpy, model_inference, top_labels=2, num_samples=1)
     
     temp, mask = explanation.get_image_and_mask(explanation.top_labels[0],
                                                 positive_only=True,
                                                 num_features=5,
                                                 hide_rest=False)
-    
+   
     # Visualize explanation
+    the_name=Path(image_path).stem # get the name of the file
     plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
     plt.axis('off')
+    plt.title(label=the_name, 
+          fontsize=20, 
+          color="green") 
     plt.show()
-    the_name=Path(image_path).stem # get the name of the file
-    the_name="Line"+the_name
+    the_name="LimeTestDataset2"+the_name
     plt.savefig('outputs/'+the_name+'.png')
-    
+ 
+
     
